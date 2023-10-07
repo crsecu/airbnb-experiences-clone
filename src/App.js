9d677724c77fe971a9fb6c8d20b348fc00061f59
Cristina Secu <secu.cristina@yahoo.com>
2023-10-06 22:17:06 -0400

    implement data driven cards

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import React from 'react';
 import Nav from './components/Nav'
 import Hero from './components/Hero'
 import Card from './components/Card'
-
+import data from './data.js'
 
 
 export default function App() {
-  // <Hero />
+  
+  const cards = data.map(item => {
+    return (
+      <Card
+       key = {item.id}
+       id = {item.id}
+       title = {item.title}
+       price = {item.price}
+       coverImg = {item.coverImg}
+       rating = {item.stats.rating}
+       reviewCount = {item.stats.reviewCount}
+       location = {item.location}
+       openSpots = {item.openSpots}
+       />
+    )
+  })
+  
   return (
     <div>
        <Nav />
-       <Card/>
-      
+       <Hero />
+       <section className='cards-list'>
+         {cards}
+       </section>
     </div>
       
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,22 @@
 import React from "react";
-import profilePicture from "../images/profile-pic.png";
 import star from "../images/star.png";
 
-export default function Card() {
+export default function Card({id, title, price, coverImg, rating, reviewCount, location, openSpots}) {
+    const checkAvailability = openSpots > 0 ? '' : "SOLD"
   return (
-    <section>
-      <a href="https://google.com" className="card">
-        <div className="card__image-container">
-            <img className = "card__image"src={profilePicture} alt="" />
-            <span className = "card__tag">SOLD OUT</span>
+      <a href="#" className="card">
+        <div className="card__image-container" id = {id}>
+            <img className = "card__image" src={`../images/${coverImg}`} alt="" />
+            {openSpots < 1 && <span className = "card__tag">SOLD OUT</span>}
         </div>
         <img className = "card__exp-star" src={star}/>
-        <span className="card__exp-rating">5.0</span>
-        <span className="card__exp-available">(6) • </span>
-        <span className="card__exp-location">USA</span>
-        <h2 className="card__exp-name">Life lessons with Katie Zaferes</h2>
-        <span className="card__exp-price">From $136</span>
+        <span className="card__exp-rating">{rating}</span>
+        <span className="card__exp-available">({reviewCount}) • </span>
+        <span className="card__exp-location">{location}</span>
+        <h2 className="card__exp-name">{title}</h2>
+        <span className="card__exp-price">From ${price}</span>
         <span>/person</span>
       </a>
-    </section>
   );
 }
+
